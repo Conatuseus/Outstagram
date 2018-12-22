f489fa284b8f091215e96333d6234d11dd72e146
conatuseus <conatuseus@gmail.com>
2018-12-23 01:03:58 +0900

    Fix Error

--- src/main/kotlin/com/conatuseus/outstagram/controller/FollowerController.kt
+++ src/main/kotlin/com/conatuseus/outstagram/controller/FollowerController.kt
@@ -1,16 +1,25 @@
 package com.conatuseus.outstagram.controller
 
 import com.conatuseus.outstagram.model.FollowerList
+import io.lettuce.core.RedisClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class FollowerController {
-    @GetMapping("/followers/{userId}")
-    fun getFollwerListByUserId(@PathVariable userId: Int) : FollowerList {
+    @GetMapping("/follower/add/{userId}")
+    fun addFollower(@PathVariable userId: Int, @RequestParam follwerId: Int): String {
+        val redisClient = RedisClient.create("localhost").connect().sync()
+        redisClient.sadd(userId.toString(), follwerId.toString())
+        return "OK"
+    }
 
+    @GetMapping("/follower/{userId}")
+    fun getFollwerListByUserId(@PathVariable userId: Int) : String {
+        val redisClient = RedisClient.create("localhost").connect().sync()
 
-        return FollowerList(listOf("Hello", "Hi"))
+        return redisClient.smembers(userId.toString()).toString()
     }
 }
--- src/main/kotlin/com/conatuseus/outstagram/services/FollowerService.kt
+++ src/main/kotlin/com/conatuseus/outstagram/services/FollowerService.kt
@@ -3,9 +3,4 @@ package com.conatuseus.outstagram.services
 import com.conatuseus.outstagram.model.FollowerList
 
 class FollowerService {
-    fun getFollowers(userId: Int) : FollowerList {
-
-    }
-
-    fun getFolloweers
 }
