b2e478a1d1b8b2c94932360374dd6bd55cf49f64
conatuseus <conatuseus@gmail.com>
2018-12-26 16:50:01 +0900

    return

--- src/main/kotlin/com/conatuseus/outstagram/controller/BojController.kt
+++ src/main/kotlin/com/conatuseus/outstagram/controller/BojController.kt
@@ -12,16 +12,13 @@ class BojController{
     @GetMapping("/BOJ/{userId}")
     fun getSolvedNumber(@PathVariable userId:String):String{
         val URL= "https://www.acmicpc.net/user/$userId"
-//        val response= Jsoup.connect(URL)
-//                .method(Connection.Method.GET)
-//                .execute()
-//
-//        val htmlDocument=response.parse()
-//        val getSolvedNumber=htmlDocument.select("#statics > tbody > tr:nth-child(2) > td > a").text()
+        val response= Jsoup.connect(URL)
+                .method(Connection.Method.GET)
+                .execute()
 
-        val doc=Jsoup.connect(URL).get()
-        val contents=doc.select("#statics > tbody > tr:nth-child(2) > td > a")
+        val htmlDocument=response.parse()
+        val getSolvedNumber=htmlDocument.select("#statics > tbody > tr:nth-child(2) > td > a").text()
 
-        return contents.toString()
+        return getSolvedNumber.toString()
     }
 }
