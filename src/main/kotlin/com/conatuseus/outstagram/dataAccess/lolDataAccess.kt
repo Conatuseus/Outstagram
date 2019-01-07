3a99f29a00cfd7e0c1bc33ccba9c00431dc5b7ec
conatuseus <conatuseus@gmail.com>
2019-01-07 22:12:52 +0900

    Added retrofit,Jackson

--- src/main/kotlin/com/conatuseus/outstagram/dataAccess/lolDataAccess.kt
+++ src/main/kotlin/com/conatuseus/outstagram/dataAccess/lolDataAccess.kt
@@ -2,6 +2,9 @@ package com.conatuseus.outstagram.dataAccess
 
 import com.conatuseus.outstagram.model.lolModel.LolUserInfo
 
-class lolDataAccess(val lolUser:LolUserInfo){
+class lolDataAccess(val lolUserId:String,val lolApiKey:String){
+    var lolUrl="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/$lolUserId?api_key=$lolApiKey"
+    fun lolGetUserInfo(){
 
+    }
 }
