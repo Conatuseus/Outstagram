eaf2d676fa8b264e368167fc658733c007b9948e
conatuseus <conatuseus@gmail.com>
2019-01-08 19:37:25 +0900

    none...

--- src/main/kotlin/com/conatuseus/outstagram/dataAccess/lolDataAccess.kt
+++ src/main/kotlin/com/conatuseus/outstagram/dataAccess/lolDataAccess.kt
@@ -1,19 +1,20 @@
 package com.conatuseus.outstagram.dataAccess
 
 import com.conatuseus.outstagram.model.lolModel.LolUserInfo
-import com.conatuseus.outstagram.services.lolService.LolGetUser
+import com.conatuseus.outstagram.services.lolService.LolUserI
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
-class lolDataAccess(val lolUserId:String,val lolApiKey:String):LolGetUser{
+class lolDataAccess(val lolUserId:String,val lolApiKey:String):LolUserI{
     override fun lolGetUserInfoByApiKey(lolUserId: String, apiKey: String): Call<LolUserInfo> {
-        val retrofit=Retrofit.Builder()
+        val retrofit:Retrofit=Retrofit.Builder()
                 .baseUrl("https://kr.api.riotgames.com")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
-        val lolGetUser:LolGetUser=retrofit.create(LolGetUser)
-
     }
+
 
     var lolAddress="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/$lolUserId?api_key=$lolApiKey"
 
--- src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolUserI.kt
+++ src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolUserI.kt
@@ -8,7 +8,7 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 
-interface LolGetUser {
+interface LolUserI {
 
     @GET("/lol/summoner/v4/summoners/by-name/{userId}?api_key={apiKey}")
     fun lolGetUserInfoByApiKey(@Path("userId") lolUserId:String,@Path("apiKey") apiKey:String):Call<LolUserInfo>
