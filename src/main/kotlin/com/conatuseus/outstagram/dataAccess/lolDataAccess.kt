03288040f8ad0165237652733d4ae253c6edcc9c
conatuseus <conatuseus@gmail.com>
2019-01-08 17:17:39 +0900

    Occur Error

--- src/main/kotlin/com/conatuseus/outstagram/dataAccess/lolDataAccess.kt
+++ src/main/kotlin/com/conatuseus/outstagram/dataAccess/lolDataAccess.kt
@@ -1,9 +1,20 @@
 package com.conatuseus.outstagram.dataAccess
 
+import com.conatuseus.outstagram.model.lolModel.LolUserInfo
+import com.conatuseus.outstagram.services.lolService.LolGetUser
+import retrofit2.Call
+import retrofit2.Retrofit
 
-class lolDataAccess(val lolUserId:String,val lolApiKey:String){
-    var lolAddress="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/$lolUserId?api_key=$lolApiKey"
-    fun lolGetUserInfo(){
+
+class lolDataAccess(val lolUserId:String,val lolApiKey:String):LolGetUser{
+    override fun lolGetUserInfoByApiKey(lolUserId: String, apiKey: String): Call<LolUserInfo> {
+        val retrofit=Retrofit.Builder()
+                .baseUrl("https://kr.api.riotgames.com")
+                .build()
+        val lolGetUser:LolGetUser=retrofit.create(LolGetUser)
 
     }
+
+    var lolAddress="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/$lolUserId?api_key=$lolApiKey"
+
 }
---
+++ src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolGetUser.kt
@@ -0,0 +1,17 @@
+package com.conatuseus.outstagram.services.lolService
+
+import com.conatuseus.outstagram.model.lolModel.LolUserInfo
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+
+interface LolGetUser {
+
+    @GET("/lol/summoner/v4/summoners/by-name/{userId}?api_key={apiKey}")
+    fun lolGetUserInfoByApiKey(@Path("userId") lolUserId:String,@Path("apiKey") apiKey:String):Call<LolUserInfo>
+
+
+}
