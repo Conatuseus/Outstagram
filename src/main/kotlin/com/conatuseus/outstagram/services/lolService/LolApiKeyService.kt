4d44aabd0de6bcb8f2a7b4cb53ea1bc9fc80456a
conatuseus <conatuseus@gmail.com>
2019-01-15 19:59:05 +0900

    Fixed baseURL

--- src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolApiKeyService.kt
+++ src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolApiKeyService.kt
@@ -1,6 +1,7 @@
 package com.conatuseus.outstagram.services.lolService
 
 
-class LolApiKeyService{
-    fun lolGetApiKey()="RGAPI-3c86f2bb-a4ee-44e6-9f4e-b138b147489c"
+class LolApiKeyService(val lolApiKey:String){
+    // val apiKey="RGAPI-3c86f2bb-a4ee-44e6-9f4e-b138b147489c"
+    fun lolGetApiKey()=lolApiKey
 }
--- src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolUserService.kt
+++ src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolUserService.kt
@@ -1,6 +1,11 @@
 package com.conatuseus.outstagram.services.lolService
 
+
 class LolUserService(val lolUserId:String,val lolApiKey:String){
 
+    fun lolGetTest(){
+        val service:
+
+    }
 
 }
