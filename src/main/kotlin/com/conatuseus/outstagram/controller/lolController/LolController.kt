2c6611395527fac6115feb9b76d57b4c5288131a
conatuseus <conatuseus@gmail.com>
2019-01-15 22:04:23 +0900

    Fix Something

--- src/main/kotlin/com/conatuseus/outstagram/controller/lolController/LolController.kt
+++ src/main/kotlin/com/conatuseus/outstagram/controller/lolController/LolController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class LolController{
 
-    @GetMapping("/lol/{user}")
+    @GetMapping("/lol/{userId}")
     fun lolGetInfoOfUser(@PathVariable userId:String):String{
 //        val apiKey="RGAPI-f6fca46d-169b-4677-899b-fe6c6de67f11"
 //        val url="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/$userId?api_key=$apiKey"
