28a7fe51e895de76a1db7617d8ae94ba3c8e0eb0
conatuseus <conatuseus@gmail.com>
2019-01-16 17:36:41 +0900

    Add something

--- src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolApiService.kt
+++ src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolApiService.kt
@@ -2,15 +2,13 @@ package com.conatuseus.outstagram.services.lolService
 
 import com.conatuseus.outstagram.model.lolModel.LolUserInfo
 import retrofit2.Call
-import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface LolApiService{
 
-    @GET("lol/summoner/v4/summoners/by-name/{lolUserId}?api_key=lolApiKey")
+    @GET("lol/summoner/v4/summoners/by-name/{lolUserId}?api_key={lolApiKey}")
     fun getUserInfo(@Path("lolUserId") lolUserId:String,
-                    @Query("lolApiKey") lolApiKey:String):Call<LolUserInfo>
+                    @Path("lolApiKey") lolApiKey:String):Call<LolUserInfo>
 
 }
--- src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolUserDAO.kt
+++ src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolUserDAO.kt
@@ -9,9 +9,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 //var lolAddress="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/$lolUserId?api_key=$lolApiKey"
-// https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/명슨?api_key=RGAPI-3c86f2bb-a4ee-44e6-9f4e-b138b147489c
+// https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/maldive?api_key=RGAPI-1600ca23-2159-4a26-a68d-28fb37028014
 class LolUserDAO(val lolUserId:String, val lolApiKey:String){
     val baseURL="https://kr.api.riotgames.com/"
+
 
     fun testRetrofit(): LolUserInfo{
         val retrofit:Retrofit=Retrofit.Builder()
@@ -20,7 +21,37 @@ class LolUserDAO(val lolUserId:String, val lolApiKey:String){
                 .build()
 
         val service= retrofit.create(LolApiService::class.java)
-        val call=service.getUserInfo(lolUserId,lolApiKey)
+        val request=service.getUserInfo(lolUserId,lolApiKey)
+        class CallBackKt<T>: Callback<T> {
+
+            var onResponse: ((Response<T>) -> Unit)? = null
+            var onFailure: ((t: Throwable?) -> Unit)? = null
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                onFailure?.invoke(t)
+            }
+
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                onResponse?.invoke(response)
+            }
+
+        }
+        fun<T> Call<T>.enqueue(callback: CallBackKt<T>.() -> Unit) {
+            val callBackKt = CallBackKt<T>()
+            callback.invoke(callBackKt)
+            this.enqueue(callBackKt)
+        }
+        request.enqueue {
+
+            onResponse = {
+
+            }
+
+            onFailure = {
+                // do
+            }
+
+        }
         return LolUserInfo(1,"a","a",1,2,"a","a")
         //임시.
     }
