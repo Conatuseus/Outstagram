4039b6f94e966a7728814b0185e14c5b745fcc72
conatuseus <conatuseus@gmail.com>
2019-01-16 17:43:40 +0900

    Fix something

--- src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolApiService.kt
+++ src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolApiService.kt
@@ -4,12 +4,11 @@ import com.conatuseus.outstagram.model.lolModel.LolUserInfo
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface LolApiService{
 
     @GET("lol/summoner/v4/summoners/by-name/{lolUserId}?api_key={lolApiKey}")
     fun getUserInfo(@Path("lolUserId") lolUserId:String,
-                    @Query("lolApiKey") lolApiKey:String):Call<LolUserInfo>
+                    @Path("lolApiKey") lolApiKey:String):Call<LolUserInfo>
 
 }
