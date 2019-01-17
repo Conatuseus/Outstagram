6c033a0901cafd558a0eca3d28ad70b8a6c7fc60
conatuseus <conatuseus@gmail.com>
2019-01-17 17:04:39 +0900

    Delete revision Date

--- src/main/kotlin/com/conatuseus/outstagram/model/lolModel/LolSummoner.kt
+++ src/main/kotlin/com/conatuseus/outstagram/model/lolModel/LolSummoner.kt
@@ -8,7 +8,6 @@ data class LolSummoner(@SerializedName("profileIconId") val profileIconId:Int,
                        @SerializedName("name") val name:String,
                        @SerializedName("puuid") val puuid:String,
                        @SerializedName("summonerLevel")val summonerLevel:Long,
-//                       @SerializedName("revisionDate") val revisionDate:Long,
                        @SerializedName("id") val id:String,
                        @SerializedName("accountId") val accountId:String
                        )
