ba3ad0f79fb3313c4e135a7676e7a2b6e6a21bcd
conatuseus <conatuseus@gmail.com>
2019-01-17 15:51:28 +0900

    Add LolMatch data class

--- src/main/kotlin/com/conatuseus/outstagram/model/lolModel/LolMatch.kt
+++ src/main/kotlin/com/conatuseus/outstagram/model/lolModel/LolMatch.kt
@@ -2,11 +2,11 @@ package com.conatuseus.outstagram.model.lolModel
 
 import com.google.gson.annotations.SerializedName
 
-data class LolMatch(@SerializedName("lane") lane:String,
-                    @SerializedName("gameId"),
-                    @SerializedName(""),
-                    @SerializedName(""),
-                    @SerializedName(""),
-                    @SerializedName(""),
-                    @SerializedName(""),
-                    @SerializedName(""))
+data class LolMatch(@SerializedName("lane") val lane:String,
+                    @SerializedName("gameId") val gameId:Long,
+                    @SerializedName("champion") val champion:Int,
+                    @SerializedName("platformId") val platformId:String,
+                    @SerializedName("timestamp") val timestamp:Long,
+                    @SerializedName("queue") val queue:Int,
+                    @SerializedName("role") val role:String,
+                    @SerializedName("season") val season:Int)
