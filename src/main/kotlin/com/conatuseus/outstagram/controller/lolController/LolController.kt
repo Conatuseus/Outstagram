abe500ad3759bbb5cd13ffd395ad263c82ed2424
conatuseus <conatuseus@gmail.com>
2019-01-17 16:34:33 +0900

    Add lolGetMatches fun in Controller

--- src/main/kotlin/com/conatuseus/outstagram/controller/lolController/LolController.kt
+++ src/main/kotlin/com/conatuseus/outstagram/controller/lolController/LolController.kt
@@ -1,5 +1,7 @@
 package com.conatuseus.outstagram.controller.lolController
 
+import com.conatuseus.outstagram.model.lolModel.LolMatch
+import com.conatuseus.outstagram.model.lolModel.LolSummoner
 import com.conatuseus.outstagram.services.lolService.LolUserService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -9,11 +11,16 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class LolController(private val lolUserService: LolUserService){
 
-    @GetMapping("/lol/{userId}")
-    fun lolGetInfoOfUser(@PathVariable userId:String):String{
+    @GetMapping("/lol/{userName}")
+    fun lolGetSummoner(@PathVariable userName:String):LolSummoner{
 //        val apiKey="RGAPI-f6fca46d-169b-4677-899b-fe6c6de67f11"
 //        val url="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/$userId?api_key=$apiKey"
-        return lolUserService.lolUserInfoService(userId).toString()
+        return lolUserService.lolGetSummonerService(userName)
+    }
+
+    @GetMapping("/lol/{userName}/matches")
+    fun lolGetMatches(@PathVariable userName: String):LolMatch{
+        return lolUserService.lolGetMatchesService(lolGetSummoner(userName).accountId)
     }
 
 
--- src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolUserService.kt
+++ src/main/kotlin/com/conatuseus/outstagram/services/lolService/LolUserService.kt
@@ -1,10 +1,14 @@
 package com.conatuseus.outstagram.services.lolService
 
+import com.conatuseus.outstagram.model.lolModel.LolMatch
 import com.conatuseus.outstagram.model.lolModel.LolSummoner
 
 class LolUserService(private val lolUserDAO: LolUserDAO){
 
-    fun lolUserInfoService(lolUserId: String): LolSummoner {
-        return lolUserDAO.getUserInfo(lolUserId)
+    fun lolGetSummonerService(lolUserName: String): LolSummoner {
+        return lolUserDAO.getLolSummoner(lolUserName)
+    }
+    fun lolGetMatchesService(lolUserName: String):LolMatch{
+        return lolUserDAO.getLolMatches(lolUserName)
     }
 }
