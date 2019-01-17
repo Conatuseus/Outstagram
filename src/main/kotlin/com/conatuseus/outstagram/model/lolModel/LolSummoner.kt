f7be2a18ee6d9f7756d616af6b1d1db3e94096c8
conatuseus <conatuseus@gmail.com>
2019-01-17 16:55:42 +0900

    Add JsonIgnoreProperties

--- src/main/kotlin/com/conatuseus/outstagram/model/lolModel/LolSummoner.kt
+++ src/main/kotlin/com/conatuseus/outstagram/model/lolModel/LolSummoner.kt
@@ -1,12 +1,14 @@
 package com.conatuseus.outstagram.model.lolModel
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.google.gson.annotations.SerializedName
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class LolSummoner(@SerializedName("profileIconId") val profileIconId:Int,
                        @SerializedName("name") val name:String,
                        @SerializedName("puuid") val puuid:String,
                        @SerializedName("summonerLevel")val summonerLevel:Long,
-                       @SerializedName("revisionDate") val revisionDate:Long,
+//                       @SerializedName("revisionDate") val revisionDate:Long,
                        @SerializedName("id") val id:String,
                        @SerializedName("accountId") val accountId:String
                        )
