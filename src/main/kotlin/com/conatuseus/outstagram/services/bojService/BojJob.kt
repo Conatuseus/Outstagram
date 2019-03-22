854071e902f3c0004736d5c4d0ee86c8d368bad9
conatuseus <conatuseus@gmail.com>
2019-03-22 14:33:03 +0900

    Add cron in Boj Service

---
+++ src/main/kotlin/com/conatuseus/outstagram/services/bojService/BojJob.kt
@@ -0,0 +1,30 @@
+package com.conatuseus.outstagram.services.bojService
+
+import io.lettuce.core.api.StatefulRedisConnection
+import org.jsoup.Connection
+import org.jsoup.Jsoup
+import org.springframework.scheduling.annotation.Scheduled
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+
+
+class BojJob(val redis:StatefulRedisConnection<String,String>){
+
+    @Scheduled(cron = "0 * * * * ?")
+    fun solvedNumJob(){
+        val list=redis.sync().smembers(USER_LIST_KEY)
+        val sc = SSLContext.getInstance("SSL")
+        sc.init(null, null, java.security.SecureRandom())
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
+
+        for(userId in list){
+            val url= "https://www.acmicpc.net/user/$userId"
+            val response= Jsoup.connect(url)
+                    .method(Connection.Method.GET)
+                    .execute()
+            val htmlDocument =response.parse()
+            val getSolvedNumber=htmlDocument.select("#statics > tbody > tr:nth-child(2) > td > a").text()
+            redis.sync().set("$SOLVED_NUM_KEY_PREFIX$userId",getSolvedNumber)
+        }
+    }
+}
